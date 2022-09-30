5aa626edfd75c35cbc3053af3034c116ed56e15b
LookHere <14829740+LookHere@users.noreply.github.com>
2022-09-30 13:32:34 -0400

    added level and compensation functionality

--- Files/LookHereDummyData.R
+++ Files/LookHereDummyData.R
@@ -1,21 +1,20 @@
 ################# PREPARE THE LOOK HERE DUMMY DATA #################
-## The "look here dummy data" generator is it's own project.  To see the other functionality here: https://github.com/LookHere/Look-Here-Dummy-Data
+## This is version 7 of the "look here dummy data" generator own project.  For the most updated code and to understand the context go here: https://github.com/LookHere/Look-Here-Dummy-Data
 
-## Please enter the number of employees you'd like it to generate, the earliest start date, and how many years of data you'd like (anyone with a term date after today is considered active).
+## Please enter the number of employees you'd like it to generate, the earliest start date, and how many years of data you'd like (anyone with a term date after today is considered active and the term date is removed).
 Headcount <- 20000
 EarliestStartDate <- "2018-01-01"
 YearsOfData <- 5
 
 ## Generates Employee ID Numbers for everyone
-## It's best practice to start headcounts at a large number so there is never a leading zero
+## For Employee IDs (sometimes known as EmplID) it's best practice to start headcounts at a very large number like 10,000,000 so there is never a leading zero
 EmployeeID <- seq(from = 10000000, to = 10000000 + Headcount -1)
 
 ## Creates a dataframe with just the employee ID numbers.  
 ## Everything will be moved into this as it's built.  
-## This will make it easier to see the data at any point.
 LHDD <- data.frame(EmployeeID)
 
-## Import categories.csv so it knows what demographics you want; header = TRUE puts the first row as the column names
+## Import categories.csv so it knows what demographics you want; header = TRUE identifies the first row as the column names
 Categories <- read.csv("D:/CompensationScience/LookHereDummyData/categories.csv", header = TRUE)
 
 
@@ -56,7 +55,7 @@ LHDD$GenderRand <- runif(Headcount, min=0, max=1)
 
 ## Defaults all records to the first demographic
 LHDD$Gender<-GenderLookup[1,1]
-## Runs a "for loop" the same number of times as there are types of demographic categories listed, starting with the second cattegory (since the first is already the category for every row)
+## Runs a "for loop" the same number of times as there are types of demographic categories listed, starting with the second category (since the first is already the category for every row)
 for (val in 2:nrow(GenderLookup))
   {
   ## Checks to see if the Random is greater than the Running Total of one record earlier; if it is overwrite that with the current demographic
@@ -94,7 +93,7 @@ for (val in 2:nrow(RaceLookup))
 LHDD <- subset(LHDD, select = - c(RaceRand))  
 
 
-################# CREATING Department RECORDS #################
+################# CREATING DEPARTMENT RECORDS #################
 ## Departments and Divisions are unique to each organization.  This is a basic example to make understanding easier.
 
 ## Create a data frame based on the ratios in the categories.csv document
@@ -121,6 +120,52 @@ for (val in 2:nrow(DepartmentLookup))
 ## Deletes the randomizer since we are done using it
 LHDD <- subset(LHDD, select = - c(DepartmentRand))  
 
+
+
+################# CREATING JOB LEVELS AND COMPENSATION #################
+## Job levels are common in organizations differentiating the hierarchy.  The compensation generated is based on a bell curve around the target pay.
+
+## Create a data frame based on the ratios in the categories.csv document
+JobLevelLookup <- data.frame(JobLevel = Categories$JobLevel, JobLevelRatio = Categories$JobLevelRatio)
+## Remove the blank rows
+JobLevelLookup <- JobLevelLookup[complete.cases(JobLevelLookup),]
+## Change the ratios from text to numbers
+JobLevelLookup$JobLevelRatio <- as.numeric(JobLevelLookup$JobLevelRatio)
+## If the ratios don't add up to 100%, this makes them add up to 100%
+JobLevelLookup$JobLevelRatio<- JobLevelLookup$JobLevelRatio/sum(JobLevelLookup$JobLevelRatio)
+## Creates a running total to compare to the randomizer
+JobLevelLookup$JobLevelRunning <- cumsum(JobLevelLookup$JobLevelRatio)
+## Creates a random variable for each employee that will decide their Department
+LHDD$JobLevelRand <- runif(Headcount, min=0, max=1)
+
+## Defaults all records to the first demographic
+LHDD$JobLevel<-JobLevelLookup[1,1]
+## Runs a "for loop" the same number of times as there are types of demographic categories listed, starting with the second cattegory (since the first is already the category for every row)
+for (val in 2:nrow(JobLevelLookup))
+{
+  ## Checks to see if the Random is greater than the Running Total of one record earlier; if it is overwrite that with the current demographic
+  LHDD$JobLevel <- ifelse(LHDD$JobLevelRand>JobLevelLookup[val-1,3], JobLevelLookup[val,1],LHDD$JobLevel)
+}
+## Deletes the randomizer since we are done using it
+LHDD <- subset(LHDD, select = - c(JobLevelRand))  
+
+## Create a lookup between JobLevel and JobLevelCompTarget
+CompTargetLookup <- subset(Categories, select = c(JobLevel, JobLevelCompTarget))
+
+## Brings in the JobLevelCompTarget
+LHDD <- merge(LHDD, CompTargetLookup,   'JobLevel')
+
+## Reorders the columns (moving JobLevel to the end) and rows (sorting by EmployeeID)
+LHDD <- LHDD[ , c( 2:ncol(LHDD),1)]
+LHDD <- LHDD[order(LHDD$"EmployeeID"),]
+
+
+## For every row, increase or decrease the compensation target by a random amount and then round to the nearest thousand
+## rnorm creates 1 random number around 0 with a standard deviation of 0.1
+for(i in 1:nrow(LHDD)) {
+  LHDD$JobLevelCompTarget[i] <- LHDD$JobLevelCompTarget[i] + LHDD$JobLevelCompTarget[i] * rnorm(1, 0, 0.1)
+  LHDD$JobLevelCompTarget[i]<- round(LHDD$JobLevelCompTarget[i], digits = -3)
+}
 
 ################# EXPORT TO CSV #################
 
